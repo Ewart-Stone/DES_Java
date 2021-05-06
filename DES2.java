94c4ab80767dacb1201c4a041ca3b6118c9dc92d
LordBlizzard98 <52279335+LordBlizzard98@users.noreply.github.com>
2021-05-06 14:55:33 +1000

    Update DES2.java

--- DES2.java
+++ DES2.java
@@ -7,8 +7,20 @@ public class DES2 extends DES0
   }
 
   @Override
+  //returns leftHalf and rightHalf
   public String[] round(String subKey, String leftHalf, String rightHalf)
   {
-
+		String leftOut = rightHalf;
+		String rightOut = "";
+		
+		rightOut = expansionPermutationE(rightHalf);
+		rightOut = xorRoundKey(rightOut, subKey);
+		rightOut = inverseExpansionPermutation(rightOut);
+		rightOut = permutationP(rightOut);
+		rightOut = xorHalves(leftHalf, rightHalf);
+		
+		String[] out = {leftOut, rightOut};
+		return out;
+		
   }
 }
