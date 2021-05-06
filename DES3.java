9a8dfbbf633475bb2bd794313e4b2ba9b0b5ce88
aparasher <74878386+aparasher@users.noreply.github.com>
2021-05-07 08:58:23 +1000

    Update DES3.java

--- DES3.java
+++ DES3.java
@@ -1,7 +1,16 @@
+/*
+ * This implements a class DES3 which shows the encryption algorithm this is the forth version of DES in this permutation P is missing from F
+ function in all rounds.
+ 
+ * Author - Edward Stone(c335050...), Ankur(c3347695) 
+ * Date - 07/06/2021 
+ * This file is used in conjuction with Assignment 2
 
-public class DES3 extends DES0
+*/
+
+public class DES3 extends DES0							// public class DES3 which  extends using DES0
 {
-  public DES3(String text, String key)
+  public DES3(String text, String key)						// we got two text and key 
   {
     super(text, key);
   }
@@ -9,15 +18,15 @@ public class DES3 extends DES0
   @Override
   public String[] round(String subKey, String leftHalf, String rightHalf)
   {
-    String leftOut = rightHalf;
-		String rightOut = "";
+    String leftOut = rightHalf;							// the left out we get is equalls to the right half
+		String rightOut = "";						//from here it is showing what are the F Functions are working in all rounds 
 		
 		rightOut = expansionPermutationE(rightHalf);
 		rightOut = xorRoundKey(rightOut, subKey);
 		rightOut = sBox(rightOut);
 		rightOut = xorHalves(leftHalf, rightHalf);
 		
-		String[] out = {leftOut, rightOut};
+		String[] out = {leftOut, rightOut};				//getting the final result 
 		return out;
   }
 }
