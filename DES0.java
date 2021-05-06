2271a31e7d984a90878fec0f3e67aa58916a6073
LordBlizzard98 <52279335+LordBlizzard98@users.noreply.github.com>
2021-05-06 13:15:52 +1000

    Update DES0.java

--- DES0.java
+++ DES0.java
@@ -41,7 +41,17 @@ public class DES0
 
     public String expansionPermutationE(String in)
     {
-
+        int[] E = {32,1,2,3,4,5,4,5,6,7,8,9,8,9,10,11,12,13,12,13,14,15,16,17,16,17,18,19,
+				   20,21,20,21,22,23,24,25,24,25,26,27,28,29,28,29,30,31,32,1};
+				   
+		String expandedString = "";
+		
+		for(int i = 0; i < 48; i++)
+		{
+			expandedString = expandedString + String.valueOf(in.charAt(E[i]));
+		}
+		
+		return expandedString;
     }
 
     public String xorRoundKey(String in, String key)
