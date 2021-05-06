683cc2cbc9bd071bec575011321b5b64d3fdae03
aparasher <74878386+aparasher@users.noreply.github.com>
2021-05-07 09:23:20 +1000

    Update DES2.java

--- DES2.java
+++ DES2.java
@@ -1,5 +1,13 @@
+/*
+ * This implements a class DES2 which shows the encryption algorithm this is the Third version of DES in this the S-boxes are missing from F Function in all rounds instead of them inverse 𝐸−1 of the Expansion permutation E is used for contraction from 48 down to 32 bits 
+ 
+ * Author - Edward Stone(c3350508), Ankur(c3347695) 
+ * Date - 07/06/2021 
+ * This file is used in conjuction with Assignment 2
 
-public class DES2 extends DES0
+*/
+
+public class DES2 extends DES0							//DES2 is a public class which extends DES0
 {
   public DES2(String text, String key)
   {
@@ -10,16 +18,16 @@ public class DES2 extends DES0
   //returns leftHalf and rightHalf
   public String[] round(String subKey, String leftHalf, String rightHalf)
   {
-		String leftOut = rightHalf;
-		String rightOut = "";
+		String leftOut = rightHalf;					// left out is equalls to right half 
+		String rightOut = "";						//here the F is working for all the rounds to get the right out 
 		
 		rightOut = expansionPermutationE(rightHalf);
 		rightOut = xorRoundKey(rightOut, subKey);
-		rightOut = inverseExpansionPermutation(rightOut);
+		rightOut = inverseExpansionPermutation(rightOut);		// instead of s-box we are using inverse expansion permutation 
 		rightOut = permutationP(rightOut);
 		rightOut = xorHalves(leftHalf, rightHalf);
 		
-		String[] out = {leftOut, rightOut};
+		String[] out = {leftOut, rightOut};				// result 
 		return out;
 		
   }
