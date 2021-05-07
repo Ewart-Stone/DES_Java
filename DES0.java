eb096bfc76cf565348782bb1b5cfe54a12d5cc2e
aparasher <74878386+aparasher@users.noreply.github.com>
2021-05-07 10:08:56 +1000

    Update DES0.java

--- DES0.java
+++ DES0.java
@@ -1,8 +1,14 @@
+/*
+ * This implements a class DES0 which shows the encryption algorithm this is the original version of DES  which shows the normal working of the F Function in all rounds 
+ * Author - Edward Stone(c3350508), Ankur(c3347695) 
+ * Date - 07/06/2021 
+ * This file is used in conjuction with Assignment 2
 
+*/
 
-public class DES0
+public class DES0									// public class DES0
 {
-    private String text;
+    private String text;								// we used 4 private strings text,key,roundkeys,cipherTexts
     private String key;
     private String[] roundkeys;
     private String[] cipherTexts;
@@ -17,16 +23,16 @@ public class DES0
       generateKeys();
     }
 
-    public void encrypt()
+    public void encrypt()								//encryption
     {
 	  String cipherText = "";
-      // initial permutation
+      											// initial permutation
 	  cipherText = initialPermutation(text);
 	  
 	  String leftHalf = cipherText.substring(0,32);
 	  String rightHalf = cipherText.substring(32);
 	  
-      // loop through 16 rounds
+     											 // loop through 16 rounds
 	  for(int i = 0; i < 16; i++)
 	  {
 		String[] temp = round(roundkeys[i], leftHalf, rightHalf);
@@ -35,12 +41,12 @@ public class DES0
 		cipherTexts[i] = leftHalf + rightHalf;
 	  }
 	  
-      //32 bit swap
+     											 //32 bit swap
 	  cipherText = rightHalf + leftHalf;
 	  
-      //inverseInitialPermutation
+      											//inverseInitialPermutation
 	  cipherText = inverseInitialPermutation(cipherText);
-      //updates this.text to encrypted text at end
+      											//updates this.text to encrypted text at end
 	  this.text = cipherText;
     }
 
@@ -48,13 +54,13 @@ public class DES0
     {
 	  
 	  String plainText = "";
-      // initial permutation
+      											// initial permutation
 	  plainText = initialPermutation(text);
 	  
 	  String leftHalf = plainText.substring(0,32);
 	  String rightHalf = plainText.substring(32);
 	  
-      // loop through 16 rounds
+     											 // loop through 16 rounds
 	  for(int i = 0; i < 16; i++)
 	  {
 		String[] temp = round(roundkeys[15 - i], leftHalf, rightHalf);
@@ -62,16 +68,16 @@ public class DES0
 		rightHalf = temp[1];
 	  }
 	  
-      //32 bit swap
+    											  //32 bit swap
 	  plainText = rightHalf + leftHalf;
 	  
-      //inverseInitialPermutation
+    											  //inverseInitialPermutation
 	  plainText = inverseInitialPermutation(plainText);
-      //updates this.text to decrypted text at end
+     											 //updates this.text to decrypted text at end
 	  this.text = plainText;
     }
 
-    //returns leftHalf and rightHalf
+    											//returns leftHalf and rightHalf
     public String[] round(String subKey, String leftHalf, String rightHalf)
     {
 		String leftOut = rightHalf;
@@ -88,7 +94,7 @@ public class DES0
 		
     }
 
-    public String expansionPermutationE(String in)
+    public String expansionPermutationE(String in)								//expansionpermutationE
     {
         int[] E = {32,1,2,3,4,5,4,5,6,7,8,9,8,9,10,11,12,13,12,13,14,15,16,17,16,17,18,19,
 				   20,21,20,21,22,23,24,25,24,25,26,27,28,29,28,29,30,31,32,1};
@@ -100,10 +106,11 @@ public class DES0
 		expandedString = expandedString + String.valueOf(in.charAt(E[i - 1]));
 	}
 
-	return expandedString;
+	return expandedString;											// return expanded string 
     }
+	
 
-    public String xorRoundKey(String in, String key)
+    public String xorRoundKey(String in, String key)								// XOR round key 
     {
 		String out = "";
 			
@@ -116,7 +123,7 @@ public class DES0
 		return out;
     }
 
-    public String sBox(String in)
+    public String sBox(String in)											// we use 8 general s-box
     {
 		int[][] S1 = {{14,0,4,15},{4,15,1,12},{13,7,14,8},{1,4,8,2},{2,14,13,4},{11,13,2,1},
 					  {8,1,11,7},{3,10,15,5},{10,6,12,11},{6,12,9,3},{12,11,7,14},{5,9,3,10},
@@ -147,8 +154,8 @@ public class DES0
 		int[][] S8 ={{13,1,7,2},{2,15,11,1},{8,13,4,14},{4,8,1,7},{6,10,9,4},{15,3,12,10},{11,7,14,8},{1,4,2,13},
 				 {10,12,0,15},{9,5,6,12},{3,6,10,9},{14,11,13,0},{5,0,15,3},{0,14,3,5},{12,9,5,6},{7,2,8,11}};
 				 
-		String[] inputSubstrings = new String[8];
-		int pos = 0;
+		String[] inputSubstrings = new String[8];										// there are 8 substrings 
+		int pos = 0;														// start postion = 0
 		
 		for(int i = 0; i < 8; i++)
 		{
@@ -212,7 +219,7 @@ public class DES0
 		return substitutedString;
 		
 	}
-    public String permutationP(String in)
+    public String permutationP(String in)							// permutationP
     {
 		int[] P = {16,7,20,21,29,12,28,17,1,15,23,26,5,18,31,10,2,8,24,14,32,27,3,
 				   9,19,13,30,6,22,11,4,25};
@@ -228,13 +235,13 @@ public class DES0
 		
     }
 
-    public String inverseExpansionPermutation(String in)
+    public String inverseExpansionPermutation(String in)					// inverse Expansion permuation used for contraction from 48 down to 32 bits
     {
 		int start = 1;
 		int end = 5;
 		String output = "";
 		
-		for(int i = 0; i < 8; i++)
+		for(int i = 0; i < 8; i++)							// loop through  8 substrings 
 		{
 			output = output + in.substring(start, end);
 			start += 6;
@@ -244,14 +251,14 @@ public class DES0
 		return output;
     }
 
-    public String initialPermutation(String in)
+    public String initialPermutation(String in)							// initial permutation 
     {
       int[] IP = {58,50,42,34,26,18,10,2,60,52,44,36,28,20,12,4,62,54,46,38,30,22,14,6,
                   64,56,48,40,32,24,16,8,57,49,41,33,25,17,9,1,59,51,43,35,27,19,11,3,
                   61,53,45,37,29,21,13,5,63,55,47,39,31,23,15,7};
 
       String permutedString = "";
-      for(int i = 0; i < 64; i++)
+      for(int i = 0; i < 64; i++)								// loop through 64 permuted strings 
       {
         permutedString = permutedString + String.valueOf(in.charAt(IP[i - 1]));
       }
@@ -259,14 +266,14 @@ public class DES0
       return permutedString;
     }
 
-    public String inverseInitialPermutation(String in)
+    public String inverseInitialPermutation(String in)						// inverse initial permutaion 
     {
       int[] IIP = {40,8,48,16,56,24,64,32,39,7,47,15,55,23,63,31,38,6,46,14,54,22,62,30,
                    37,5,45,13,53,21,61,29,36,4,44,12,52,20,60,28,35,3,43,11,51,19,59,27,
                    34,2,42,10,50,18,58,26,33,1,41,9,49,17,57,25};
 
       String permutedString = "";
-      for(int i = 0; i < 64; i++)
+      for(int i = 0; i < 64; i++)								// loop through 64 
       {
         permutedString = permutedString + String.valueOf(in.charAt(IIP[i - 1]));
       }
@@ -274,9 +281,9 @@ public class DES0
       return permutedString;
     }
 
-    public String xorHalves(String leftHalf, String rightHalf)
+    public String xorHalves(String leftHalf, String rightHalf)				
     {
-      //xor righthalf with lefthalf
+      												//xor righthalf with lefthalf
 	  
 		String out = "";
 			
@@ -290,7 +297,7 @@ public class DES0
 
     }
 
-    public void generateKeys()
+    public void generateKeys()							 // generate general key 
     {
       String leftHalf;
       String rightHalf;
@@ -300,13 +307,13 @@ public class DES0
       leftHalf = halves[0];
       rightHalf = halves[1];
 
-      for(int i = 0; i < 16; i++)
+      for(int i = 0; i < 16; i++)						// loop through 16 bits
       {
          leftHalf = leftShift(leftHalf, i + 1);
          rightHalf = leftShift(rightHalf, i + 1);
          this.roundkeys[i] = permutedChoice2(leftHalf, rightHalf);
-         //
-         //
+         
+         
       }
     }
 
@@ -317,9 +324,9 @@ public class DES0
       int[] PC1 = {57,49,41,33,25,17,9,1,58,50,42,34,26,18,10,2,59,51,43,35,27,19,11,3,60,52,44,36,
                    63,55,47,39,31,23,15,7,62,54,46,38,30,22,14,6,61,53,45,37,29,21,13,5,28,20,12,4};
 
-      //permuted this.key into two halves
-      //leftHalf at index 0
-      //rightHalf at index 1
+      													//permuted this.key into two halves
+      													//leftHalf at index 0
+     													 //rightHalf at index 1
 
       for(int i = 0; i < 56; i++)
       {
@@ -340,7 +347,7 @@ public class DES0
     {
       String shiftedHalf = "";
 
-      //shift by one bit
+      												//shift by one bit
       if(round == 1 || round == 2 || round == 9 || round == 16)
       {
         for(int i = 1; i < half.length(); i++)
@@ -350,7 +357,7 @@ public class DES0
 
         shiftedHalf = shiftedHalf + String.valueOf(half.charAt(0));
       }
-      //shift by two bits
+      												//shift by two bits
       else
       {
         for(int i = 2; i < half.length(); i++)
@@ -371,9 +378,9 @@ public class DES0
       String subkey = "";
 
       int[] PC2 = {14,17,11,24,1,5,3,28,15,6,21,10,23,19,12,4,26,8,16,7,27,20,13,2,
-                   41,52,31,37,47,55,30,40,51,45,33,48,44,49,39,56,34,53,46,42,50,36,29,32};
+                   41,52,31,37,47,55,30,40,51,45,33,48,44,49,39,56,34,53,46,42,50,36,29,32};	//pc-2
 
-      for(int i = 0; i < 48; i++)
+      for(int i = 0; i < 48; i++)				  				//loop through 48 bits key
       {
         subkey = subkey + String.valueOf(joinedHalves.charAt(PC2[i - 1]));
       }
@@ -381,19 +388,19 @@ public class DES0
       return subkey;
     }
 
-    public String getCipherText(int index)
+    public String getCipherText(int index)			// get cipher text
     {
-      return this.cipherTexts[index];
+      return this.cipherTexts[index];				// return 
     }
 
-    public String getText()
+    public String getText()					// get text
     {
-      return this.text;
+      return this.text;						// return text
     }
 
-    public String getKey()
+    public String getKey()					// get key
     {
-      return this.key;
+      return this.key;						// return key
     }
 
 
